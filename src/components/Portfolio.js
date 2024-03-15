5cc359c6b83348c6e9b85c92435f4457e23a84ad
kitzing <slkitzing@gmail.com>
2024-03-15 11:29:47 +0100

    add portfolio items

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,12 +3,21 @@ import PortfolioItem from './helpers/PortfolioItem.js';
 import pic from '../resources/placeholder.jpg'
 
 function Portfolio() {
+    const portfolioData = [
+        {name: 'Designing for Maritime Coordination', date: 'Spring 2021', info: 'Master thesis done at the Interaction design and technologies master. The master thesis was a collaboration with the Swedish Sea Rescue Society, where we investigated how to design a coordination system for maritime rescue.', image: pic, moreImages: [pic, pic, pic]},
+        {name: 'Exercise motivation', date: 'Autumn 2020', info: 'An 8 weeklong individual project at Chalmers University of Technology where I came up with an idea and fulfilled it by myself. The research was about designing for motivation to workout.', image: pic, moreImages: [pic, pic, pic]},
+        {name: 'Disgeyesed', date: 'Autumn 2020', info: 'A project for the course Interaction design project at Chalmers University of Technology. The project was a collaboration with Universeum on the topic of Quantified self.', image: pic, moreImages: [pic, pic, pic]},
+        {name: 'Keychain', date: 'Autumn 2020', info: 'A project for the course Mobile Computing at Chalmers University of Technology. The goal was to design a new, innovative action on the theme of authentication and access.', image: pic, moreImages: [pic, pic, pic]},
+        {name: 'Let´s talk sugar', date: 'Spring 2020', info: 'A project for the course Information visualization at Chalmers University of Technology. The idea was to provide information for any subject in an informative way. Our project focused on inform about sugar consumption and how it affects you.', image: pic, moreImages: [pic, pic, pic]},
+        {name: 'Wärdshuset Spinnaren', date: 'Autumn 2019', info: 'Individual project for the course Graphical Interfaces at Chalmers University of Technology. The purpose was to design a system for scheduling and room booking for a small hotel. The hotel was 1800s themed, including clothes to wear from that time. Figma was used to create this design.', image: pic, moreImages: [pic, pic, pic]},
+        {name: 'Argama', date: 'Spring 2018', info: 'Bachelor thesis done with 5 other students in the Software Engineering program. The purpose was to develop a web application for students where they could create a game by writing a game plan in plain English.', image: pic, moreImages: [pic, pic, pic]},
+    ]
     return (
         <div id="portfolio" className='portfolio-section'>
             <h1 className='header'>PORTFOLIO</h1>
-            <PortfolioItem name={'Extercise motivation'} date={'Autumn 2020'} image={pic} moreImages={[pic, pic, pic]} info={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'} left={true} />
-            <PortfolioItem name={'Title 1'} date={'Summer 2022'} image={pic} moreImages={[pic, pic, pic]}  info={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'} />
-            <PortfolioItem name={'Title 1'} date={'Summer 2022'} image={pic} moreImages={[pic, pic, pic]}  info={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'} left={true} />
+            {portfolioData.map((item, index) => (
+                    <PortfolioItem item={item} left={!(index % 2)} key={index}/>
+                    ))}
         </div>
     );}
 
--- src/components/helpers/PortfolioItem.js
+++ src/components/helpers/PortfolioItem.js
@@ -5,7 +5,7 @@ import PortfolioItemOpen from './PortfolioItemOpen.js';
 import PortfolioInfo from './PortfolioInfo.js';
 import ResponsiveImage from './ResponsiveImage.js';
 
-function PortfolioItem({ name, date, image, moreImages, info, left }) {
+function PortfolioItem({ item, left }) {
   const [isOpen, setIsOpen] = useState(false)
   if(left){
     return (
@@ -15,14 +15,14 @@ function PortfolioItem({ name, date, image, moreImages, info, left }) {
               <Col sm={6}>
                 <div className='portfolio-image'>
                   <ResponsiveImage
-                      src={image}
+                      src={item.image}
                       width={ 500 }
                       height={ 300 } />
                 </div>
               </Col>
               <Col sm={6} style={{display: 'flex'}}>
                 <div style={{marginTop: 'auto'}}>
-                    <PortfolioInfo name={name} date={date} info={info} left={true} />
+                    <PortfolioInfo name={item.name} date={item.date} info={item.info} left={true} />
                     <div style={{display: 'flex', justifyContent: 'center'}}>
                       <Button variant='dark' onClick={() => setIsOpen(!isOpen)} aria-controls='collapseId'>{isOpen ? 'Close' : 'See more'}</Button>
                     </div>
@@ -32,7 +32,7 @@ function PortfolioItem({ name, date, image, moreImages, info, left }) {
           </Container>
           <Collapse in={isOpen}>
             <div id='collapseId'>
-              <PortfolioItemOpen images={moreImages} />
+              <PortfolioItemOpen images={item.moreImages} />
             </div>
           </Collapse>
       </div>     
@@ -44,7 +44,7 @@ function PortfolioItem({ name, date, image, moreImages, info, left }) {
           <Row>
             <Col sm={{span: 6, order: 'first'}} xs={{ order: 'last' }} md={{ order: 'first' }} style={{display: 'flex'}}>
               <div style={{marginTop: 'auto'}}>
-                <PortfolioInfo name={name} date={date} info={info} left={false} />
+                <PortfolioInfo name={item.name} date={item.date} info={item.info} left={false} />
                 <div style={{display: 'flex', justifyContent: 'center'}}>
                   <Button variant='dark' onClick={() => setIsOpen(!isOpen)} aria-controls='collapseId'>{isOpen ? 'Close' : 'See more'}</Button>
                 </div>
@@ -52,7 +52,7 @@ function PortfolioItem({ name, date, image, moreImages, info, left }) {
             </Col>
                 <Col sm={6}>
                 <ResponsiveImage
-                    src={image}
+                    src={item.image}
                     width={ 500 }
                     height={ 300 } />
                 </Col>
@@ -60,7 +60,7 @@ function PortfolioItem({ name, date, image, moreImages, info, left }) {
         </Container>
         <Collapse in={isOpen}>
           <div id='collapseId'>
-            <PortfolioItemOpen images={moreImages} />
+            <PortfolioItemOpen images={item.moreImages} />
           </div>
         </Collapse>
       </div>
